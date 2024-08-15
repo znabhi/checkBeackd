673d5e3068984ea391df9f6e80364d21bfb87596
Abhishek Jain <67307608+znabhi@users.noreply.github.com>
2024-08-15 21:19:23 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 // Create Express app
 const app = express();
 
@@ -14,7 +14,7 @@ const app = express();
 //   })
 // );
 app.use(cors()); 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 // Middleware for parsing JSON and URL-encoded data
 app.use(express.json({ limit: "20kb" })); // Limit JSON payload size
 app.use(express.urlencoded({ extended: true, limit: "20kb" })); // Limit URL-encoded payload size
