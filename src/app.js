52a0461c4ff2e3ab42d7af7fc606e265f6189152
Abhishek Jain <67307608+znabhi@users.noreply.github.com>
2024-08-15 18:13:12 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 //     credentials: true, // Allow sending cookies
 //   })
 // );
-app.use(cors());
+app.use(cors()); 
 app.use(bodyParser.json());
 // Middleware for parsing JSON and URL-encoded data
 app.use(express.json({ limit: "20kb" })); // Limit JSON payload size
