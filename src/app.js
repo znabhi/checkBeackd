1270eb4e6bf1dc0c3548a16be3ada5af7f27c278
Abhishek Jain <67307608+znabhi@users.noreply.github.com>
2024-08-15 18:19:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import bodyParser from 'body-parser';
 // Create Express app
 const app = express();
 
